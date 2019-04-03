84f5a81dabb0a2d75cfce4faa86b19a75d6b6333
Randle <35815963+nrandle3@users.noreply.github.com>
2019-04-03 13:23:55 -0500

    Finalized numbers for the encounter probability

--- Genv2.py
+++ Genv2.py
@@ -6,26 +6,32 @@ pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
 df = pd.read_csv("./EncountersTransposed.csv")
+
+def value_counts(arr):
+    unique_elements, counts_elements = np.unique(arr, return_counts=True)
+    return (np.asarray((unique_elements, counts_elements)))
+
 def genEnc(danger):
-        
-    danger = 4
+    danger = min(danger,5)
+    
     
     types = df["Type"].unique()
-    
-    weights = { "Character Encounter":1,
-               "Friendly Social":1*int(danger/2),
-               "Skill Check":1*danger,
-               "Hostile Social":1*danger,
-               "Combat (Non-committal)":1*(danger),
-               "Combat (Aggressive)":1*(danger-1)
+
+    weights = { "Character Encounter":1+ (5-danger),
+               "Friendly Social":1 + (5-danger),
+               "Skill Check":danger,
+               "Hostile Social":danger,
+               "Combat (Non-committal)":danger,
+               "Combat (Aggressive)":max(1,(danger-2))**2
             
             }
+    typesWeighted = []
     
     for i,encType in enumerate(types):
         if encType in weights:
             for j in range(weights[encType]):
-                types = np.append(types,encType)
-            
-    return rn.choice(types)
+                
+                typesWeighted = np.append(typesWeighted,encType)
+    return rn.choice(typesWeighted)
 
-print(genEnc(1))
+
